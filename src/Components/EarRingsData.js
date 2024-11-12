b167b7259bc12f8c13e78abdb02f446940fdc2e8
althaf11 <84334262+althaf11@users.noreply.github.com>
2024-11-13 00:36:48 +0530

    Update EarRingsData.js

--- src/Components/EarRingsData.js
+++ src/Components/EarRingsData.js
@@ -1,373 +1,373 @@
 const EarRingsData = [
     {
       id: 1,
-      title: "Gold Bangle",
+      title: "EarRings",
       price: "$1500",
       image: "../public/Card/e1.jpg" // Make sure the path matches your project directory
     },
     {
       id: 2,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e2.jpg"
     },
     {
       id: 3,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e3.jpg"
     },
     {
       id: 4,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e4.jpg"
     },
     {
       id: 5,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e5.jpg"
     },
     {
       id: 6,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e6.jpg"
     },
     {
       id: 7,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e7.jpg"
     },
     {
       id: 8,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e8.jpg"
     },
     {
       id: 9,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e9.jpg"
     },
     {
       id: 10,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e10.jpg"
     },
     {
       id: 11,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e11.jpg"
     },
     {
       id: 12,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e12.jpg"
     },
     {
       id: 13,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e13.jpg"
     },
     {
       id: 14,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e14.jpg"
     },
     {
       id: 15,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e15.png"
     },
     {
       id: 16,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e16.png"
     },
     {
       id: 17,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e17.png"
     },
     {
       id: 18,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e18.png"
     },
     {
       id: 19,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e19.png"
     },
     {
       id: 20,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e20.png"
     },
     {
       id: 21,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e21.png"
     },
     {
       id: 22,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e22.png"
     },
     {
       id: 23,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e23.png"
     },
     {
       id: 24,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e24.jpg"
     },
     {
       id: 25,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e25.png"
     },
     {
       id: 26,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e26.png"
     },
     {
       id: 27,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e27.png"
     },
     {
       id: 28,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e28.jpg"
     },
     {
       id: 29,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e29.jpg"
     },
     {
       id: 30,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e30.jpg"
     },
     {
       id: 31,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e31.jpg"
     },
     {
       id: 32,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e32.jpg"
     },
     {
       id: 33,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e34.jpg"
     },
     {
       id: 34,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e34.jpg"
     },
     {
       id: 35,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e35.jpg"
     },
     {
       id: 36,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e36.jpg"
     },
     {
       id: 37,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e37.jpg"
     },
     {
       id: 38,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e38.jpg"
     },
     {
       id: 39,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e39.jpg"
     },
     {
       id: 40,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e40.jpg"
     },
     {
       id: 41,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e41.jpg"
     },
     {
       id: 42,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e42.jpg"
     },
     {
       id: 43,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e43.jpg"
     },
     {
       id: 44,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e44.jpg"
     },
     {
       id: 45,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e45.jpg"
     },
     {
       id: 46,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e46.jpg"
     },
     {
       id: 47,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e47.jpg"
     },
     {
       id: 48,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e48.jpg"
     },
     {
       id: 49,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e49.jpg"
     },
     {
       id: 50,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e50.jpg"
     },
     {
       id: 51,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e51.jpg"
     },
     {
       id: 52,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e52.jpg"
     },
     {
       id: 53,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e53.jpg"
     },
     {
       id: 54,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e54.jpg"
     },
     {
       id: 55,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e55.jpg"
     },
     {
       id: 56,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e56.jpg"
     },
     {
       id: 57,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e57.jpg"
     },
     {
       id: 58,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e58.jpg"
     },
     {
       id: 59,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e59.jpg"
     },
     {
       id: 60,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e60.jpg"
     },
     {
       id: 61,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e61.jpg"
     },
     {
       id: 62,
-      title: "Silver Bangle",
+      title: "EarRings",
       price: "$800",
       image: "../public/Card/e62.jpg"
     },
