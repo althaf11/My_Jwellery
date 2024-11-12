15a3346698ae3311a212e142f3db1480ee3c86ad
althaf11 <84334262+althaf11@users.noreply.github.com>
2024-11-13 00:40:35 +0530

    Update VaddanamData.js

--- src/Components/VaddanamData.js
+++ src/Components/VaddanamData.js
@@ -7,463 +7,463 @@ const VaddanamData = [
       },
       {
         id: 2,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v2.jpg"
       },
       {
         id: 3,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v3.jpg"
       },
       {
         id: 4,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v4.jpg"
       },
       {
         id: 5,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v5.jpg"
       },
       {
         id: 6,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v6.jpg"
       },
       {
         id: 7,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v7.jpg"
       },
       {
         id: 8,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v8.jpg"
       },
       {
         id: 9,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v9.jpg"
       },
       {
         id: 10,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v10.jpg"
       },
       {
         id: 11,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v11.jpg"
       },
       {
         id: 12,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v12.jpg"
       },
       {
         id: 13,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v13.jpg"
       },
       {
         id: 14,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v14.jpg"
       },
       {
         id: 15,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v15.jpg"
       },
       {
         id: 16,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v16.jpg"
       },
       {
         id: 17,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v17.jpg"
       },
       {
         id: 18,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v18.jpg"
       },
       {
         id: 19,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v19.jpg"
       },
       {
         id: 20,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v20.jpg"
       },
       {
         id: 21,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v21.jpg"
       },
       {
         id: 22,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v22.jpg"
       },
       {
         id: 23,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v23.jpg"
       },
       {
         id: 24,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v24.jpg"
       },
       {
         id: 25,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v25.jpg"
       },
       {
         id: 26,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v26.jpg"
       },
       {
         id: 27,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v27.jpg"
       },
       {
         id: 28,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v28.jpg"
       },
       {
         id: 29,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v29.jpg"
       },
       {
         id: 30,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v30.jpg"
       },
       {
         id: 31,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v31.jpg"
       },
       {
         id: 32,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v32.jpg"
       },
       {
         id: 33,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v34.jpg"
       },
       {
         id: 34,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v34.jpg"
       },
       {
         id: 35,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v35.jpg"
       },
       {
         id: 36,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v36.jpg"
       },
       {
         id: 37,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v37.jpg"
       },
       {
         id: 38,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v38.jpg"
       },
       {
         id: 39,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v39.jpg"
       },
       {
         id: 40,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v40.jpg"
       },
       {
         id: 41,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v41.jpg"
       },
       {
         id: 42,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v42.jpg"
       },
       {
         id: 43,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v43.jpg"
       },
       {
         id: 44,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v44.jpg"
       },
       {
         id: 45,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v45.jpg"
       },
       {
         id: 46,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v46.jpg"
       },
       {
         id: 47,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v47.jpg"
       },
       {
         id: 48,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v48.jpg"
       },
       {
         id: 49,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v49.jpg"
       },
       {
         id: 50,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v50.jpg"
       },
       {
         id: 51,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v51.jpg"
       },
       {
         id: 52,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v52.jpg"
       },
       {
         id: 53,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v53.jpg"
       },
       {
         id: 54,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v54.jpg"
       },
       {
         id: 55,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v55.jpg"
       },
       {
         id: 56,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v56.jpg"
       },
       {
         id: 57,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v57.jpg"
       },
       {
         id: 58,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v58.jpg"
       },
       {
         id: 59,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v59.jpg"
       },
       {
         id: 60,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v60.jpg"
       },
       {
         id: 61,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v61.jpg"
       },
       {
         id: 62,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v62.jpg"
       },
       {
         id: 63,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v63.jpg"
       },
       {
         id: 64,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v64.jpg"
       },
       {
         id: 65,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v65.jpg"
       },
       {
         id: 66,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v66.jpg"
       },
       {
         id: 67,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v67.jpg"
       },
       {
         id: 68,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v68.jpg"
       },
       {
         id: 69,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v69.jpg"
       },
       {
         id: 70,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v70.jpg"
       },
       {
         id: 71,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v71.jpg"
       },
       {
         id: 72,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v72.jpg"
       },
       {
         id: 73,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v73.jpg"
       },
       {
         id: 74,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v74.jpg"
       },
       {
         id: 75,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v75.jpg"
       },
       {
         id: 76,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v76.jpg"
       },
       {
         id: 77,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v77.jpg"
       },
       {
         id: 78,
-        title: "Silver Bangle",
+        title: "Vaddanam",
         price: "$800",
         image: "../public/Card/v78.jpg"
       },
